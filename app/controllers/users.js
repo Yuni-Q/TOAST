a4e4b16b1932be1c016bdaa820e3fddcf2ae9fdc
Yuni-Q <carnotcycle2@naver.com>
2019-04-01 08:36:40 +0900

    fb

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -176,6 +176,7 @@ router.get('/:id', isLoggedIn, async (req, res) => {
 //   // https://developers.kakao.com/docs/restapi/user-management#%EC%82%AC%EC%9A%A9%EC%9E%90-%EC%A0%95%EB%B3%B4-%EC%9A%94%EC%B2%AD
 //       console.log('kakao', response);
 //     } else {
+  // https://graph.facebook.com/me?fields=user_age_range,user_gender,id,name,email&access_token=EAAfr4OqZCqEkBABz2EvcKbRLQDmWTuqSHQ7PcfEfTKlPSffha89RJCMX4Pf4615TS0vPjAywEHjdqBs92kuHhgn3G5Ym3j2X5cUG2RvlUL8gh8Jrz5CI2y57bpLwFNP0SsldbNXHZCVUjXYZAVc0LOxgkKRk5zQXFuZA4pEZAvStI7c10UXhZAcgS627enqSKegyeskIGDnL93giqtGJPYEcobVP8tzyMvmTRRc4NIGQZDZD
 //         let response = await request(`https://graph.facebook.com/me?access_token=${req.body.access_token}`, {
 //         // This example demonstrates all of the supported options.
 //         // Request method (uppercase): POST, DELETE, ...
