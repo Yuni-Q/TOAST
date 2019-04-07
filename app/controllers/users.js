0dedd7b66569886414d466381cf35342027f63a4
Yuni-Q <carnotcycle2@naver.com>
2019-04-07 22:18:07 +0900

    sns

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const uuidv1 = require('uuid/v1');
+const request = require('sync-request');
 
 const {
   resultFormat,
@@ -15,6 +16,43 @@ const {
 } = require('../models');
 
 const router = express.Router();
+
+router.put('/sns', isNotLoggedIn, async (req, res) => {
+  let response = {
+    nickName: null,
+    email: null,
+    gender: null,
+    year: null,
+  };
+  try {
+    if (req.body.sns === 'kakao') {
+      const kakaoResponse = await request('https://kapi.kakao.com/v2/user/me', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${req.body.accessToken}`,
+          },
+      });
+      response.nickName = kakaoResponse.properties.nickname;
+      response.email = kakaoResponse.kakao_account.email;
+      response.gender = kakaoResponse.kakao_account.gender;
+      res.json(resultFormat(true, null, response));
+      return;
+    } else {
+      const facebookResponse = await request(`https://graph.facebook.com/me?access_token=${req.body.accessToken}&field=id,name,gender,birthday`, {
+      });
+      response.nickName = facebookResponse.name;
+      response.email = facebookResponse.email;
+      response.year = (facebookResponse.birthday).split('/')[2];
+      response.gender = facebookResponse.gender;
+      res.json(resultFormat(true, null, response));
+      return;
+    }
+  } catch (error) {
+    res.json(resultFormat(false, error.message));
+    return;
+  }
+  res.json(resultFormat(false, 'kakao나 facebook이 아닙니다.'));
+});
 
 router.post("/email", async (req, res, next) => {
   const email = req.body.email;
@@ -127,88 +165,5 @@ router.get('/:id', isLoggedIn, async (req, res) => {
     res.json(resultFormat(false, error.message));
   }
 });
-
-router.put('/sns', isLoggedIn, async (req, res) => {
-  try {
-//     // response.statusCode
-//     // response.headers
-//     // response.body
-    if (req.body.sns === 'kakao') {
-//       let response = await request('https://kapi.kakao.com/v2/user/me', {
-//           // This example demonstrates all of the supported options.
-//           // Request method (uppercase): POST, DELETE, ...
-//           method: 'GET',
-//           // data: {
-//           //   properties: {"nickname":"홍길동","age":"22"},
-//           // },
-//           headers: {
-//             Authorization: `Bearer ${req.body.access_token}`,
-//             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-//           },
-//           // proxy: 'http://127.0.0.1:8000',
-//           // To create a new cookie jar.
-//           // cookieJar: true,
-//           // To use a custom/existing cookie jar.
-//           // https://www.npmjs.com/package/tough-cookie
-//           // cookieJar: new tough.CookieJar()
-//       });
-// //       키	설명	필수
-// // property_keys	사용자 정보의 키 리스트. JSON Array 형태.	X
-// // secure_resource	이미지 url을 https로 반환할지 여부. true/false	X
-// // property_keys로 요청할 수 있는 키 리스트입니다.
-
-// // 설정 > 사용자 관리 > 사용자 목록 및 프로퍼티 관리 에서 추가로 등록한 프로퍼티도 사용할 수 있습니다.
-
-// // Name	Description
-// // properties.nickname	카카오톡 또는 카카오스토리의 닉네임 정보
-// // properties.profile_image	640px * 640px 크기의 프로필 이미지 URL (2017/08/22 이전 가입자에 대해서는 480px * 480px ~ 1024px * 1024px 크기를 가질 수 있음)
-// // properties.thumbnail_image	110px * 110px 크기의 썸네일 프로필 이미지 URL (2017/08/22 이전 가입자에 대해서는 160px * 213px 크기를 가질 수 있음)
-// // kakao_account.email	사용자 카카오계정의 이메일 소유여부, 이메일 값, 이메일 인증여부, 이메일 유효여부
-// // kakao_account.age_range	사용자 카카오계정의 연령대 소유여부, 연령대 값
-// // kakao_account.birthday	사용자 카카오계정의 생일 소유여부, 생일 값
-// // kakao_account.gender	사용자 카카오계정의 성별 소유여부, 성별 값
-
-// // curl -v -X POST https://kapi.kakao.com/v2/user/me \
-// //   -H "Authorization: KakaoAK kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" \
-// //   -d 'target_id_type=user_id&target_id=12345' \
-//   // -d 'property_keys=["kakao_account.email"]'
-
-//   // https://developers.kakao.com/docs/restapi/user-management#%EC%82%AC%EC%9A%A9%EC%9E%90-%EC%A0%95%EB%B3%B4-%EC%9A%94%EC%B2%AD
-//       console.log('kakao', response);
-    } else {
-  // https://graph.facebook.com/me?fields=user_age_range,user_gender,id,name,email&access_token=EAAfr4OqZCqEkBABz2EvcKbRLQDmWTuqSHQ7PcfEfTKlPSffha89RJCMX4Pf4615TS0vPjAywEHjdqBs92kuHhgn3G5Ym3j2X5cUG2RvlUL8gh8Jrz5CI2y57bpLwFNP0SsldbNXHZCVUjXYZAVc0LOxgkKRk5zQXFuZA4pEZAvStI7c10UXhZAcgS627enqSKegyeskIGDnL93giqtGJPYEcobVP8tzyMvmTRRc4NIGQZDZD
-        let response = await request(`https://graph.facebook.com/me?access_token=${req.body.access_token}&field=id,name,gender,birthday`, {
-//         // This example demonstrates all of the supported options.
-//         // Request method (uppercase): POST, DELETE, ...
-//         method: 'GET',
-//         // data: {
-//         //   properties: {"nickname":"홍길동","age":"22"},
-//         // },
-//         // headers: {
-//         //   Authorization: `Bearer ${access_token}`,
-//         //   "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-//         // },
-//         // proxy: 'http://127.0.0.1:8000',
-//         // To create a new cookie jar.
-//         // cookieJar: true,
-//         // To use a custom/existing cookie jar.
-//         // https://www.npmjs.com/package/tough-cookie
-//         // cookieJar: new tough.CookieJar()
-    });
-
-
-    console.log('facebook', response);
-      // return getJsonPojo(httpEntity, ResFacebookLoginPojo.class);
-
-    }
-  } catch (error) {
-    res.json(resultFormat(false, error.message));
-    return;
-  }
-  res.json(resultFormat(false, 'kakao나 facebook이 아닙니다.'));
-});
-
-
-
 
 module.exports = router;
