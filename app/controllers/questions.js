cb6eac0e34952c65ac7b010a9e0df0d3dc51d127
Yuni-Q <carnotcycle2@naver.com>
2019-04-21 15:34:39 +0900

    feat: add local toast backup

--- app/controllers/questions.js
+++ app/controllers/questions.js
@@ -175,6 +175,22 @@ router.get('/:id', isLoggedIn, async (req, res) => {
       userId: req.user.id,
     },
   });
+  if (parseInt(req.params.id, 10) === 0) {
+    const query = `
+      select
+        * 
+      from questions 
+        join toasts on questions.id = toasts.questionId
+        left join (select count(*) as keepsCount, toastId from keeps JOIN toasts on toasts.id = keeps.toastId ) as keeps on keeps.toastId = toasts.id
+        left join (select count(*) as alertCount, toastId from alerts JOIN toasts on toasts.id = alerts.toastId ) as alerts on alerts.toastId = toasts.id
+        where questions.id = ${req.params.id};
+      `;
+    const result = await db.sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+    res.json(resultFormat(true, null, result));
+  }
+
   if (share) {
     const query = `
       select
@@ -190,6 +206,7 @@ router.get('/:id', isLoggedIn, async (req, res) => {
       type: sequelize.QueryTypes.SELECT,
     });
     res.json(resultFormat(true, null, result));
+    return;
   } else {
     const query = `
       select
@@ -205,7 +222,9 @@ router.get('/:id', isLoggedIn, async (req, res) => {
       type: sequelize.QueryTypes.SELECT,
     });
     res.json(resultFormat(true, null, result));
+    return;
   }
+  res.json(resultFormat(false, 'return 되지 못했습니다.', result));
 });
 
 
